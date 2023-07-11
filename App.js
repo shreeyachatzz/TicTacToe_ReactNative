d91a9f1f7593be0fdd677116c40aea2ff6e2a87a
Shreeya Chatterji <91721717+shreeyachatzz@users.noreply.github.com>
2023-07-12 00:57:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StyleSheet, View } from 'react-native';
+import Game from './src/game';
+
+import {LinearGradient} from 'expo-linear-gradient';
+
+export default function App() {
+  return (
+    <LinearGradient
+      colors={['#3498db','#ffffff']}
+      style={styles.container}
+    >
+      <View style={styles.overlay}>
+        <Game />
+      </View>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  overlay:{
+    flex:1,
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    justifyContent:'center',
+    alignItems:'center'
+  }
+});
